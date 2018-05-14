811a11b2658fb68aec41dcaa5f641a2e8933edca
unknown <mrklimatomas@gmail.com>
2018-05-14 22:11:47 +0200

    attempt to fix mongo

--- db.js
+++ db.js
@@ -5,17 +5,9 @@ module.exports = {
   getTodos
 };
 
-if (process.env.NODE_ENV === "production") {
-  console.log("PRODUCTION ENVIRONMENT");
-  const username = process.env.DB_USERNAME;
-  const password = process.env.DB_PASSWORD;
-  const dburl = process.env.DB_URL;
-} else {
-  console.log("fallback to placeholder db");
-  const username = "username";
-  const password = "password";
-  const dburl = "mongodb://localhost:27017/react-ts-todolist";
-}
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const dburl = process.env.DB_URL;
 
 const todoSchema = mongoose.Schema({
   completed: Boolean,
